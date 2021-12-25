f957b675d8598ceffaa6da6fa72439bff7b8b2e9
Edu <jeduardogl655@gmail.com>
2021-12-24 19:20:46 -0600

    launcher paths to run server analisis

--- launchers/launcher.sh
+++ launchers/launcher.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 cd ../
 
+mirnabed="/bodega/projects/miRNome_project/references/beds/hsa_mature_miRNAs.bed"
+utrbed="/bodega/projects/miRNome_project/references/beds/3utr.bed"
+vcf="/bodega/projects/miRNome_project/references/vcfs/LXXVIg_PASS_AF_OV.vcf.gz"
+fasta="/bodega/projects/miRNome_project/references/fastas/"
+output_directory="/bodega/projects/miRNome_project/analisis/100GMX/results"
 
-input_mirna="real-data/chrom22_mut/CHROM22.mirna.fa"
-input_utr="real-data/chrom22_ref/CHROM22.utr.fa"
-output_directory="real-data/chrom22_mut/results-2"
-
-nextflow run compare-miRNA-pairs.nf \
-	--mirna_fa $input_mirna \
-	--utr_fa	$input_utr \
+echo -e "======\n Lauching NF execution \n======" \
+&& rm -rf $output_directory \
+&& nextflow run nf-compare-miRNome.nf \
+	--mirnabed $mirnabed \
+  --utrbed $utrbed \
+	--vcf $vcf \
+	--fasta_dir $fasta \
 	--output_dir $output_directory \
 	-resume \
 	-with-report $output_directory/`date +%Y%m%d_%H%M%S`_report.html \
-	-with-dag $output_directory/`date +%Y%m%d_%H%M%S`.DAG.html
+	-with-dag $output_directory/`date +%Y%m%d_%H%M%S`.DAG.html \
+&& echo -e "======\n  pipeline execution END \n======"
