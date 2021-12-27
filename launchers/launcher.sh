37660d3abf6e4c4da2409cbc6c923057d0be511b
Edu <jeduardogl655@gmail.com>
2021-12-27 13:48:15 -0600

    launcher paths to run server analisis

--- launchers/launcher.sh
+++ launchers/launcher.sh
@@ -10,12 +10,12 @@ output_directory="/bodega/projects/miRNome_project/analisis/100GMX/results"
 echo -e "======\n Lauching NF execution \n======" \
 && rm -rf $output_directory \
 && nextflow run nf-compare-miRNome.nf \
-	--mirnabed $mirnabed \
+        --mirnabed $mirnabed \
   --utrbed $utrbed \
-	--vcf $vcf \
-	--fasta_dir $fasta \
-	--output_dir $output_directory \
-	-resume \
-	-with-report $output_directory/`date +%Y%m%d_%H%M%S`_report.html \
-	-with-dag $output_directory/`date +%Y%m%d_%H%M%S`.DAG.html \
+        --vcf $vcf \
+        --fasta_dir $fasta \
+        --output_dir $output_directory \
+        -resume \
+        -with-report $output_directory/`date +%Y%m%d_%H%M%S`_report.html \
+        -with-dag $output_directory/`date +%Y%m%d_%H%M%S`.DAG.html \
 && echo -e "======\n  pipeline execution END \n======"
