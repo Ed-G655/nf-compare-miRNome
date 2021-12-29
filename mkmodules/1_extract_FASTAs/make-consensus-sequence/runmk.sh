8e6a8cefaa5d90be915178fd18f65b00230aa3e2
Edu <jeduardogl655@gmail.com>
2021-12-29 14:15:57 -0600

    Change input file to vcf.gz

--- mkmodules/1_extract_FASTAs/make-consensus-sequence/runmk.sh
+++ mkmodules/1_extract_FASTAs/make-consensus-sequence/runmk.sh
@@ -4,6 +4,6 @@
 #find: -L option to include symlinks
 find -L . \
   -type f \
-  -name "*.vcf" \
-| sed "s#.vcf#.fa.consensus#" \
+  -name "*.vcf.gz" \
+| sed "s#.vcf.gz#.fa.consensus#" \
 | xargs mk
