c4d285ae8fd0773863d4c6008e88bc6b8c3dc0cd
Edu <jeduardogl655@gmail.com>
2022-04-13 15:42:39 -0500

    Change input launcher

--- launchers/launcher.sh
+++ launchers/launcher.sh
@@ -1,21 +1,21 @@
 ##!/usr/bin/env bash
 cd ../
 
-mirnabed="/bodega/projects/miRNome_project/references/beds/hsa_mature_miRNAs.bed"
+mirna_gff="/bodega/projects/miRNome_project/references/gff/hsa.gff3"
 utrbed="/bodega/projects/miRNome_project/references/beds/3utr.bed"
 vcf="/bodega/projects/miRNome_project/references/vcfs/76g_PASS_0_05.vcf.gz"
 fasta="/bodega/projects/miRNome_project/references/fastas/"
 output_directory="/bodega/projects/miRNome_project/analisis/100GMX/results"
 
 echo -e "======\n Lauching NF execution \n======" \
-&& rm -rf $output_directory \
 && nextflow run nf-compare-miRNome.nf \
-        --mirnabed $mirnabed \
-  --utrbed $utrbed \
+        --mirna_gff $mirna_gff \
+        --utrbed $utrbed \
         --vcf $vcf \
         --fasta_dir $fasta \
         --output_dir $output_directory \
         -resume \
         -with-report $output_directory/`date +%Y%m%d_%H%M%S`_report.html \
         -with-dag $output_directory/`date +%Y%m%d_%H%M%S`.DAG.html \
+        -with-timeline $output_directory/`date +%Y%m%d_%H%M%S`_timeline.html \
 && echo -e "======\n  pipeline execution END \n======"
